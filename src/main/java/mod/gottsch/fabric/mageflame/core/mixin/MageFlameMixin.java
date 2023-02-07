a4239cdd79f0afffa02976538308807dd78a9873
gottsch <mark.gottschling@gmail.com>
2023-02-07 15:34:07 -0500

    Added ability to set light in all-air chunk section AND remove block wireframes.

---
+++ src/main/java/mod/gottsch/fabric/mageflame/core/mixin/MageFlameMixin.java
@@ -0,0 +1,40 @@
+/*
+ * This file is part of  Mage Flame.
+ * Copyright (c) 2023 Mark Gottschling (gottsch)
+ *
+ * Mage Flame is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Mage Flame is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Mage Flame.  If not, see <http://www.gnu.org/licenses/lgpl>.
+ */
+package mod.gottsch.fabric.mageflame.core.mixin;
+
+import mod.gottsch.fabric.mageflame.core.block.ISummonFlameBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(WorldRenderer.class)
+public class MageFlameMixin {
+    @Inject(method = "drawBlockOutline", at = @At("HEAD"), cancellable = true)
+    private void onDrawBlockOutline(MatrixStack matrixStack, VertexConsumer vertexConsumer, Entity entity, double cameraX, double cameraY, double cameraZ, BlockPos blockPos, BlockState blockState, CallbackInfo ci) {
+        if (blockState.getBlock() instanceof ISummonFlameBlock) {
+            ci.cancel();
+        }
+    }
+}
