1790b0d8cbe86eb8efb06f820cccf40fce1f2b02
gottsch <17928819+gottsch@users.noreply.github.com>
2025-01-08 08:13:57 -0500

    initial build

--- src/main/java/mod/gottsch/fabric/mageflame/core/client/renderer/entity/GreaterRevelationRenderer.java
+++ src/main/java/mod/gottsch/fabric/mageflame/core/client/renderer/entity/GreaterRevelationRenderer.java
@@ -6,12 +6,13 @@ import mod.gottsch.fabric.mageflame.core.entity.creature.GreaterRevelationEntity
 import mod.gottsch.fabric.mageflame.core.setup.ClientSetup;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.MobEntityRenderer;
+import net.minecraft.client.render.entity.state.LivingEntityRenderState;
 import net.minecraft.util.Identifier;
 
 /*
  * A renderer is used to provide an entity model, shadow size, and texture.
  */
-public class GreaterRevelationRenderer<T extends GreaterRevelationEntity> extends MobEntityRenderer<T, LargeFlameBallModel<T>> {
+public class GreaterRevelationRenderer<T extends GreaterRevelationEntity> extends MobEntityRenderer<T, LivingEntityRenderState, LargeFlameBallModel> {
 
     public GreaterRevelationRenderer(EntityRendererFactory.Context context) {
         super(context, new LargeFlameBallModel<>(context.getPart(ClientSetup.LARGE_FLAME_BALL_LAYER)), 0.5f);
